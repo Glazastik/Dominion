f2fca205844add884782cf0286e393d612bbde06
Glazastik <anton.myrholm@gmail.com>
2013-03-21 09:29:30 +0100

    Updated the ServerFrame for basic structure.

--- src/se/chalmers/tda367/dominion/server/ServerFrame.java
+++ src/se/chalmers/tda367/dominion/server/ServerFrame.java
@@ -1,10 +1,31 @@
 package se.chalmers.tda367.dominion.server;
 
 import javax.swing.JFrame;
+import javax.swing.JTextArea;
 
 public class ServerFrame extends JFrame {
 
+	//Frame-related variables
 	private static final long serialVersionUID = -6536441362330626938L;
+	private static final String FRAMETITLE = "Dominion Server 0.1"; 
+	
+	//List of components:
+	private JTextArea consoleOut;
+	
+	/**
+	 * Constructor for the server frame.
+	 * Initiates the components.
+	 */
+	public ServerFrame(){
+		super(FRAMETITLE);
+		initComponents();
+	}
+
+	private void initComponents() {
+		
+		
+	}
+	
 	
 	
 }
