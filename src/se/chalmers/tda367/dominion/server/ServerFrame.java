f28cd8e5e528350babccd4f5fcf0575b4a42ba30
Glazastik <anton.myrholm@gmail.com>
2013-03-21 09:23:41 +0100

    Added ServerFrame.java

---
+++ src/se/chalmers/tda367/dominion/server/ServerFrame.java
@@ -0,0 +1,10 @@
+package se.chalmers.tda367.dominion.server;
+
+import javax.swing.JFrame;
+
+public class ServerFrame extends JFrame {
+
+	private static final long serialVersionUID = -6536441362330626938L;
+	
+	
+}
