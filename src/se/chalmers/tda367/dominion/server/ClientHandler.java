5a986d83675f86a60b5ec7968861d76e93711766
Malm <malmstenen@gmail.com>
2013-03-21 16:02:05 +0100

    Added javadoc

--- src/se/chalmers/tda367/dominion/server/ClientHandler.java
+++ src/se/chalmers/tda367/dominion/server/ClientHandler.java
@@ -1,5 +1,9 @@
 package se.chalmers.tda367.dominion.server;
 
+/**
+ * A class that handles the clients 
+ * @author Grupp 28
+ */
 public class ClientHandler {
 
 }
--- src/se/chalmers/tda367/dominion/server/RoomHandler.java
+++ src/se/chalmers/tda367/dominion/server/RoomHandler.java
@@ -1,5 +1,9 @@
 package se.chalmers.tda367.dominion.server;
 
+/**
+ * A class for managing all the rooms on the server
+ * @author Grupp 28
+ */
 public class RoomHandler {
 
 }
