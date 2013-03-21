365805bf40133f23395e58cdb909457227236ba0
Glazastik <anton.myrholm@gmail.com>
2013-03-21 09:42:40 +0100

    Added a layout manager to ServerFrame

--- src/se/chalmers/tda367/dominion/server/ServerFrame.java
+++ src/se/chalmers/tda367/dominion/server/ServerFrame.java
@@ -1,5 +1,7 @@
 package se.chalmers.tda367.dominion.server;
 
+import java.awt.GridLayout;
+
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
@@ -9,7 +11,12 @@ public class ServerFrame extends JFrame {
 	private static final long serialVersionUID = -6536441362330626938L;
 	private static final String FRAMETITLE = "Dominion Server 0.1"; 
 	
+	
 	//List of components:
+	
+	/**
+	 * Serves as the main source of output for the server.
+	 */
 	private JTextArea consoleOut;
 	
 	/**
@@ -21,8 +28,12 @@ public class ServerFrame extends JFrame {
 		initComponents();
 	}
 
+	/**
+	 * Initiates the required components for the JFrame.
+	 */
 	private void initComponents() {
-		
+		//The layout
+		this.setLayout(new GridLayout(1,2));
 		
 	}
 	
--- src/se/chalmers/tda367/dominion/server/ServerMain.java
+++ src/se/chalmers/tda367/dominion/server/ServerMain.java
@@ -15,7 +15,7 @@ public class ServerMain {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new ServerFrame();
 
 	}
 
