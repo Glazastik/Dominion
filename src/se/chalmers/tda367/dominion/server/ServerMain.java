73449e656f6895918c4034229096a38fed5e3d68
Glazastik <anton.myrholm@gmail.com>
2013-03-21 09:16:49 +0100

    Added proper package and main class for server.

---
+++ src/se/chalmers/tda367/dominion/server/ServerMain.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package se.chalmers.tda367.dominion.server;
+
+/**
+ * @author Anton Myrholm
+ * 
+ *
+ */
+public class ServerMain {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
