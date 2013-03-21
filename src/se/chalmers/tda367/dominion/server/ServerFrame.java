4b9443c315e10887e3af0d9ad7166012ab0d4d8f
Glazastik <anton.myrholm@gmail.com>
2013-03-21 10:15:01 +0100

    Updated the server with appropriate settings, fixed server frame and changed the colors.

--- src/se/chalmers/tda367/dominion/server/ServerFrame.java
+++ src/se/chalmers/tda367/dominion/server/ServerFrame.java
@@ -1,30 +1,45 @@
 package se.chalmers.tda367.dominion.server;
 
-import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class ServerFrame extends JFrame {
 
-	//Frame-related variables
+	// Frame-related variables
 	private static final long serialVersionUID = -6536441362330626938L;
-	private static final String FRAMETITLE = "Dominion Server 0.1"; 
-	
-	
-	//List of components:
-	
+	private static final String FRAMETITLE = "Dominion Server 0.1";
+	private static final int FRAMEHEIGHT = 400;
+	private static final int FRAMEWIDTH = 600;
+
+	// List of components:
+
 	/**
 	 * Serves as the main source of output for the server.
 	 */
 	private JTextArea consoleOut;
-	
+
 	/**
-	 * Constructor for the server frame.
-	 * Initiates the components.
+	 * Serves as the main input source for the server.
 	 */
-	public ServerFrame(){
+	private JTextField consoleIn;
+
+
+	/**
+	 * Constructor for the server frame. Initiates the components.
+	 */
+	public ServerFrame() {
 		super(FRAMETITLE);
+		this.setSize(FRAMEWIDTH, FRAMEHEIGHT);
+
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
 		initComponents();
 	}
 
@@ -32,11 +47,44 @@ public class ServerFrame extends JFrame {
 	 * Initiates the required components for the JFrame.
 	 */
 	private void initComponents() {
-		//The layout
-		this.setLayout(new GridLayout(1,2));
-		
+		// The layout
+		this.setLayout(new BorderLayout());
+		this.setBackground(Color.BLACK);
+
+		consoleOut = new JTextArea();
+		consoleOut.setEditable(false);
+		consoleOut.setForeground(Color.WHITE);
+		consoleOut.setBackground(Color.BLACK);
+
+
+		consoleIn = new JTextField();
+		consoleIn.setForeground(Color.WHITE);
+		consoleIn.setBackground(Color.BLACK);
+
+		this.add(consoleOut, BorderLayout.CENTER);
+		this.add(consoleIn, BorderLayout.PAGE_END);
+
+		this.validate();
+		this.repaint();
+		consoleIn.requestFocus();
 	}
 	
+	/**
+	 * Appends the server frame with the text.
+	 * @param text
+	 */
+	public void print(String text){
+		consoleOut.append("<" + getTime() + "> " + text);
+	}
 	
-	
+	/**
+	 * Method for getting the current time in a relevant format
+	 */
+	private String getTime(){
+		Calendar cal = Calendar.getInstance();
+    	cal.getTime();
+    	SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+    	return sdf.format(cal.getTime());
+	}
+
 }
--- src/se/chalmers/tda367/dominion/server/ServerMain.java
+++ src/se/chalmers/tda367/dominion/server/ServerMain.java
@@ -10,13 +10,16 @@ package se.chalmers.tda367.dominion.server;
  */
 public class ServerMain {
 
+	private static ServerFrame window;
+	
 	/**
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new ServerFrame();
+		window = new ServerFrame();
 
+		window.print("Server initiated...");
 	}
 
 }
