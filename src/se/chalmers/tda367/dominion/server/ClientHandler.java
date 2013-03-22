e786ffd103bf465c3c50892223d8c8e08e63d103
Glazastik <anton.myrholm@gmail.com>
2013-03-22 16:42:25 +0100

    Modified ClientHandler to have more serverlike features.

--- src/se/chalmers/tda367/dominion/server/ClientHandler.java
+++ src/se/chalmers/tda367/dominion/server/ClientHandler.java
@@ -1,16 +1,30 @@
 package se.chalmers.tda367.dominion.server;
 
+import java.net.ServerSocket;
 import java.util.List;
 
 /**
- * A class that handles the clients 
- * @author Grupp 28
+ * A class that handles the clients
+ * 
+ * @author Group 28
  */
 public class ClientHandler {
-	
+	// Constants:
+	private final int SERVER_PORT = 1337;
+	private final int TIMEOUT = 30000;
+
+	/**
+	 * List of clients the server has connected.
+	 */
 	private List<Client> clientList;
-	
+
+	/**
+	 * The main part of the connectivity of the server, the actual socket.
+	 */
+	private static ServerSocket serverSocket;
+
 	public ClientHandler() {
-		
+
 	}
+
 }
