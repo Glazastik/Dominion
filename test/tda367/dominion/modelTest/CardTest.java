f580be54f43c1b07883fd959a2f3b976156b89a9
Dover <doverstav@hotmail.com>
2013-04-11 10:02:20 +0200

    Added empty testcase for the Card-class

---
+++ test/tda367/dominion/modelTest/CardTest.java
@@ -0,0 +1,24 @@
+package tda367.dominion.modelTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CardTest {
+
+	@Test
+	public void testCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetImage() {
+		fail("Not yet implemented");
+	}
+
+}
