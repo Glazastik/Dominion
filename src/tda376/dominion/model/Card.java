f719e099219421199fd15946800ba80d750d6472
Wiixtor <wiixtor@gmail.com>
2013-04-11 09:26:21 +0200

    Added constructor and javadoc for Card

--- src/tda376/dominion/model/Card.java
+++ src/tda376/dominion/model/Card.java
@@ -1,8 +1,18 @@
 package tda376.dominion.model;
 
+/**
+ * 
+ * The class that represents the cards.
+ * @author Group 28
+ *
+ */
 public class Card {
 	//private final Image image;
 	private final String name;
 	
+	public Card(String name) {
+		this.name = name;
+		//this.image = image
+	}
 	//public Card get() {}
 }
