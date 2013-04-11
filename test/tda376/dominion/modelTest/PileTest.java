041c35dfaa33a9057589592a66d35eb60589ee37
Dover <doverstav@hotmail.com>
2013-04-11 11:17:46 +0200

    Added empty testcases for the Pile-class

---
+++ test/tda376/dominion/modelTest/PileTest.java
@@ -0,0 +1,44 @@
+package tda376.dominion.modelTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PileTest {
+
+	@Test
+	public void testPile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPileListOfCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPop() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testShuffle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCards() {
+		fail("Not yet implemented");
+	}
+
+}
