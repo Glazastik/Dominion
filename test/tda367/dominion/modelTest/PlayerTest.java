e991bcde9dc8f0716331063a52ef10b7ad7710a9
Dover <doverstav@hotmail.com>
2013-04-11 10:00:19 +0200

    Added empty testcase for the Player-class

---
+++ test/tda367/dominion/modelTest/PlayerTest.java
@@ -0,0 +1,64 @@
+package tda367.dominion.modelTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PlayerTest {
+
+	@Test
+	public void testPlayer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDraw() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDrawInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDiscardHand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDiscardCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddToHand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRevealHand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRevealTopOfHand() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDiscardFromDeck() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPutOnTopOfDeck() {
+		fail("Not yet implemented");
+	}
+
+}
