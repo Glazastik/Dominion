a522defb90656bc571ab90a41a87a96a87f0311b
Wiixtor <wiixtor@gmail.com>
2013-04-15 16:15:39 +0200

    Added test for discardFromDeck() and putOnTopOfDeck()

--- test/tda376/dominion/modelTest/PlayerTest.java
+++ test/tda376/dominion/modelTest/PlayerTest.java
@@ -74,12 +74,18 @@ public class PlayerTest {
 
 	@Test
 	public void testDiscardFromDeck() {
-		fail("Not yet implemented");
+		Player p = new Player("Test");
+		//Not sure if this is how discardFromDeck should work
+		//p.discardFromDeck(1);
+		p.discardFromDeck("Copper");
+		assertTrue(p.getDeckSize() == 4);
 	}
 
 	@Test
 	public void testPutOnTopOfDeck() {
-		fail("Not yet implemented");
+		Player p = new Player("Test");
+		p.putOnTopOfDeck("Curse");
+		assertTrue(p.getDeckSize() == 6);
 	}
 
 }
