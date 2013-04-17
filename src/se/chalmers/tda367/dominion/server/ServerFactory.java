507e78ed1b26a5ad9226e68a6d59f371bacd61a2
Glazastik <anton.myrholm@gmail.com>
2013-04-17 11:17:40 +0200

    ServerFactory can now return instace of server.

--- src/se/chalmers/tda367/dominion/server/ServerFactory.java
+++ src/se/chalmers/tda367/dominion/server/ServerFactory.java
@@ -1,9 +1,35 @@
 package se.chalmers.tda367.dominion.server;
 
-public class ServerFactory {
+import java.io.IOException;
 
-	public ServerFactory(){
+import com.esotericsoftware.kryonet.Server;
+
+public class ServerFactory {
+	private static Server server;
+
+	public ServerFactory() {
+
+	}
+
+	public static synchronized/**
+								 * Creates and returns a fresh instance of the server
+								 * @return
+								 */
+	public static synchronized Server getInstance() {
+		if (server != null) {
+			return server;
+		}
+
+		server = new Server();
+
+		try {
+			server.bind(54555, 54777);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
+		return server;
+
 	}
 
 }
