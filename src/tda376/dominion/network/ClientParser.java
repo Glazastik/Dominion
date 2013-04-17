7b66ead87323e8fe54a5d2f3525aaa6f716199cc
Meddan <christoffer.medin@gmail.com>
2013-04-17 10:25:12 +0200

    Renamed clientcontroller to clientparser.

---
+++ src/tda376/dominion/network/ClientParser.java
@@ -0,0 +1,16 @@
+package tda376.dominion.network;
+
+/**
+ * A class that listens to datatraffic from the clients
+ * and calls the correct method in Dominion. 
+ * 
+ * @author grupp 28
+ */
+public class ClientParser {
+	
+	public ClientParser() {
+		
+	}
+	
+	
+}
