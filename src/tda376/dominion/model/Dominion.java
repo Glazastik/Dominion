37cd021f77a8249115bd1fbd6a2860a3934f2889
Pontus Doverstav <doverstav@hotmail.com>
2013-04-18 11:27:51 +0200

    Added javadoc for Dominion

--- src/tda376/dominion/model/Dominion.java
+++ src/tda376/dominion/model/Dominion.java
@@ -24,15 +24,40 @@ public class Dominion {
 		gainingHandler = new GainingHandler(supply);
 		cardRulesHandler = new CardRulesHandler(players, gainingHandler);
 	}
+	
+	/**
+	 * Returns a list of players (see {@link Player}) 
+	 * that are playing the game.
+	 * 
+	 * @return a list containing the players
+	 */
 	public LinkedList<Player> getPlayers(){
 		return this.players;
 	}
+	
+	/**
+	 * Returns the {@link Supply} used in the game.
+	 * 
+	 * @return the {@link Supply}
+	 */
 	public Supply getSupply(){
 		return this.supply;
 	}
+	
+	/**
+	 * Returns the {@link GainingHandler} used in the game.
+	 * 
+	 * @return the {@link GainingHandler}
+	 */
 	public GainingHandler getGainingHandler(){
 		return this.gainingHandler;
 	}
+	
+	/**
+	 * Returns the {@link CardRulesHandler} used in the game.
+	 * 
+	 * @return the {@link CardRulesHandler}
+	 */
 	public CardRulesHandler getCardRulesHandler(){
 		return this.cardRulesHandler;
 	}
