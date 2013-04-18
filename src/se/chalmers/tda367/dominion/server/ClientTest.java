f165d252d7df218cf9248f52202d79d537bd6b25
Glazastik <anton.myrholm@gmail.com>
2013-04-18 10:33:51 +0200

    Added ClientTest.java (temporary) to test the features of the server.

---
+++ src/se/chalmers/tda367/dominion/server/ClientTest.java
@@ -0,0 +1,61 @@
+package se.chalmers.tda367.dominion.server;
+
+import java.io.IOException;
+
+import net.java.games.input.EventQueue;
+
+import com.esotericsoftware.kryonet.Client;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
+
+/**
+ * Basic client for connecting to the kryo server.
+ * 
+ * @author Glazastik
+ * 
+ */
+public class ClientTest {
+	static Client client;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		client = new Client();
+		
+
+		client.addListener(new Listener() {
+			public void connected(Connection connection) {
+				client.sendTCP("Hej");
+			}
+
+			public void disconnected(Connection connection) {
+				System.out.println("Fuuu");
+			}
+		});
+
+		client.start();
+		new Thread("Connect") {
+			public void run() {
+				try {
+					client.connect(5000, "localhost", 54555, 54777);
+					// Server communication after connection can go here, or in
+					// Listener#connected().
+				} catch (IOException ex) {
+					ex.printStackTrace();
+					System.exit(1);
+				}
+			}
+		}.start();
+
+		// try {
+		// Log.set(Log.LEVEL_DEBUG);
+		// client.connect(5000, "localhost", 54555, 54777);
+		// System.out.println(client.isConnected());
+		// } catch (IOException e) {
+		// e.printStackTrace();
+		// }
+
+	}
+
+}
--- src/se/chalmers/tda367/dominion/server/ServerFactory.java
+++ src/se/chalmers/tda367/dominion/server/ServerFactory.java
@@ -2,6 +2,8 @@ package se.chalmers.tda367.dominion.server;
 
 import java.io.IOException;
 
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.kryonet.Server;
 
 public class ServerFactory {
@@ -19,13 +21,23 @@ public class ServerFactory {
 
 		server = new Server();
 
+		server.addListener(new Listener() {
+			public void received(Connection c, Object obj) {
+
+				ServerFrame.getInstance().print(
+						c.getRemoteAddressTCP().toString() + " > "
+								+ (String) obj);
+			}
+		});
+
 		try {
 			server.bind(54555, 54777);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+		server.start();
 		return server;
+		
 
 	}
 
