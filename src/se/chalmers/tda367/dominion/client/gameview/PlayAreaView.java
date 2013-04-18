873f157b4f9cacf2a6b03e6629e646209c613309
Glazastik <anton.myrholm@gmail.com>
2013-04-18 11:25:50 +0200

    Deleted old package gameview

--- src/se/chalmers/tda367/dominion/client/gameview/PlayAreaView.java
+++
@@ -1,9 +0,0 @@
-package se.chalmers.tda367.dominion.client.gameview;
-/**
- * 
- * @author christoffer
- * A class used to represent the action cards and treasures in play
- */
-public class PlayAreaView {
-
-}
