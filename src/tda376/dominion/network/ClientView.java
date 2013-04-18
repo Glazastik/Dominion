45737fa4ec03982c08a5c164634d176012daf6e9
Malm <malmstenen@gmail.com>
2013-04-18 13:37:09 +0200

    Added methods for ClientView

--- src/tda376/dominion/network/ClientView.java
+++ src/tda376/dominion/network/ClientView.java
@@ -1,5 +1,25 @@
 package tda376.dominion.network;
 
+/**
+ * A class that sends information to the client
+ */
 public class ClientView {
 	
+	public ClientView() {
+		
+	}
+	
+	/**
+	 * Let the player play a card
+	 */
+	public void playCard() {
+		
+	}
+	
+	/**
+	 * Tells the client to discard a card
+	 */
+	public void discardCard(String card) {
+		
+	}
 }
