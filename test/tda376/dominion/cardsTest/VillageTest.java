7a3cc3275719790d656b63ad2c9c80819e02a7d0
Wiixtor <wiixtor@gmail.com>
2013-04-22 11:39:23 +0200

    Complemented VillageTest

--- test/tda376/dominion/cardsTest/VillageTest.java
+++ test/tda376/dominion/cardsTest/VillageTest.java
@@ -3,12 +3,22 @@ package tda376.dominion.cardsTest;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import tda376.dominion.model.*;
+import tda376.dominion.cards.Village;
 
 public class VillageTest {
 
 	@Test
 	public void testPlay() {
-		fail("Not yet implemented");
+		Player p = new Player("HNNNG");
+		int actions = p.getActions();
+		int handsize = p.getHandSize();
+		Village.play(p);
+		int actionsDiff = p.getActions() - actions;
+		int handsizeDiff = p.getHandSize() - handsize;
+		assertTrue(actionsDiff == 2);
+		assertTrue(handsizeDiff == 1);
+		
 	}
 
 }
