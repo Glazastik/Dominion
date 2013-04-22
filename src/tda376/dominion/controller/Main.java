c55f570f324d299680718d304096b9dc5f219c7c
Glazastik <anton.myrholm@gmail.com>
2013-04-22 11:11:13 +0200

    Updated Main.java

--- src/tda376/dominion/controller/Main.java
+++ src/tda376/dominion/controller/Main.java
@@ -5,23 +5,26 @@ import com.esotericsoftware.kryonet.Server;
 import se.chalmers.tda367.dominion.server.ServerFactory;
 import se.chalmers.tda367.dominion.server.ServerFrame;
 import tda376.dominion.model.Dominion;
-
+/**
+ * The main executable class for this project.
+ * Will start a server which the clients can connect to.
+ * @author Group 28
+ *
+ */
 public class Main {
 	private static Dominion dominion;
 	private static ServerFrame window;
 	
 	/**
-	 * Main executable class for the Dominion game
+	 * Main method.
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		window = ServerFrame.getInstance();
-		window.print("Frame initiated.");
+		window.print("Window initiated.");
 		
 		Server server = ServerFactory.getInstance();
-		window.print("Server service started.");
-		System.out.println(server.getConnections().length);
-		
+		window.print("Server now running.");
 		
 		//dominion = new Dominion(, );
 		
