aa70bcaec011b4b12c97b55f7a0682ad8af6b725
Meddan <christoffer.medin@gmail.com>
2013-04-22 22:11:04 +0200

    Added docummentation.

--- src/tda367/dominion/messages/CreateBoolMessage.java
+++ src/tda367/dominion/messages/CreateBoolMessage.java
@@ -1,7 +1,16 @@
 package tda367.dominion.messages;
 
+/**
+ * A message class used to tell the view to create a yes/no option
+ * @author christoffer
+ *
+ */
 public class CreateBoolMessage implements Message {
 	private String message;
+	/**
+	 * The constructor
+	 * @param message the message for the view to display i.e. "Put on top of deck?"
+	 */
 	public CreateBoolMessage(String message){
 		this.message = message;
 	}
