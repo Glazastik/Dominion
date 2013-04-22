8a21a5674a766ec4a054e3c0baa246a435ed4a13
Wiixtor <wiixtor@gmail.com>
2013-04-22 11:07:15 +0200

    Created test package for cards and all the current classes in cards

---
+++ test/tda376/dominion/cardsTest/AdventurerTest.java
@@ -0,0 +1,14 @@
+package tda376.dominion.cardsTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AdventurerTest {
+
+	@Test
+	public void testPlay() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ test/tda376/dominion/cardsTest/LaboratoryTest.java
@@ -0,0 +1,14 @@
+package tda376.dominion.cardsTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LaboratoryTest {
+
+	@Test
+	public void testPlay() {
+		fail("Not yet implemented");
+	}
+
+}
