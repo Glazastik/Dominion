e1c3130995d81b3fbaf2ae2d8c988a8f5bd456ba
Wiixtor <wiixtor@gmail.com>
2013-04-22 12:32:09 +0200

    Complemented FestivalTest

--- test/tda376/dominion/cardsTest/FestivalTest.java
+++ test/tda376/dominion/cardsTest/FestivalTest.java
@@ -4,11 +4,21 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import tda376.dominion.cards.Festival;
+import tda376.dominion.model.Player;
+
 public class FestivalTest {
 
 	@Test
 	public void testPlay() {
-		fail("Not yet implemented");
+		Player p = new Player("Ragnar");
+		int buys = p.getBuys();
+		int actions = p.getActions();
+		int money = p.getMoney();
+		Festival.play(p);
+		assertTrue(p.getBuys() - buys == 1);
+		assertTrue(p.getActions() - actions == 2);
+		assertTrue(p.getMoney() - money == 2);
 	}
 
 }
