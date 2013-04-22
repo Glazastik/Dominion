e2ca4d51233c40761facd17c13924593015f6a91
Wiixtor <wiixtor@gmail.com>
2013-04-22 11:28:27 +0200

    Complemented WitchTest and WoodcutterTest

--- test/tda376/dominion/cardsTest/WoodcutterTest.java
+++ test/tda376/dominion/cardsTest/WoodcutterTest.java
@@ -1,14 +1,17 @@
 package tda376.dominion.cardsTest;
 
 import static org.junit.Assert.*;
-
+import tda376.dominion.cards.Woodcutter;
+import tda376.dominion.model.*;
 import org.junit.Test;
 
 public class WoodcutterTest {
 
 	@Test
 	public void testPlay() {
-		fail("Not yet implemented");
+		Player p = new Player("Chuck");
+		Woodcutter.play(p);
+		assertTrue(p.getBuys() == 2 && p.getMoney() == 2);
 	}
 
 }
