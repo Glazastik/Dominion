f5318359d62a790e03ca519a6737888cf2070216
Wiixtor <wiixtor@gmail.com>
2013-04-22 12:40:36 +0200

    Complemented CouncilroomTest

--- test/tda376/dominion/cardsTest/CouncilroomTest.java
+++ test/tda376/dominion/cardsTest/CouncilroomTest.java
@@ -2,13 +2,33 @@ package tda376.dominion.cardsTest;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 import org.junit.Test;
+
+import tda376.dominion.cards.Councilroom;
+import tda376.dominion.model.Player;
 
 public class CouncilroomTest {
 
 	@Test
 	public void testPlay() {
-		fail("Not yet implemented");
+		Player p1 = new Player("Markolio");
+		Player p2 = new Player("Knugen");
+		Player p3 = new Player("Bacchus");
+		LinkedList<Player> players = new LinkedList<Player>();
+		players.add(p1);
+		players.add(p2);
+		players.add(p3);
+		int p1Cards = p1.getHandSize();
+		int p1Buys = p1.getBuys();
+		int p2Cards = p2.getHandSize();
+		int p3Cards = p3.getHandSize();
+		Councilroom.play(p1, players);	
+		assertTrue(p1.getBuys() - p1Buys == 1);
+		assertTrue(p1.getHandSize() - p1Cards == 4);
+		assertTrue(p2.getHandSize() - p2Cards == 1);
+		assertTrue(p3.getHandSize() - p3Cards == 1);
 	}
 
 }
