a0b67f238140a7f79fbd4674b7b6cf45437e3ea6
Wiixtor <wiixtor@gmail.com>
2013-04-22 12:17:01 +0200

    Complemented MoatTest

--- test/tda376/dominion/cardsTest/MoatTest.java
+++ test/tda376/dominion/cardsTest/MoatTest.java
@@ -1,14 +1,20 @@
 package tda376.dominion.cardsTest;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import tda376.dominion.cards.Moat;
+import tda376.dominion.model.Player;
 
 public class MoatTest {
 
 	@Test
 	public void testPlay() {
-		fail("Not yet implemented");
+		Player p = new Player("Mr.Boat");
+		int cards = p.getHandSize();
+		Moat.play(p);
+		assertTrue(p.getHandSize() - cards == 2);
 	}
 
 }
