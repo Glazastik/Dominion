903c8a10bee382c5881870de6840c120a1c896cf
Glazastik <anton.myrholm@gmail.com>
2013-04-22 11:19:23 +0200

    Added the NetworkCommon.java, more info on this file on the wiki

---
+++ src/se/chalmers/tda367/dominion/server/NetworkCommon.java
@@ -0,0 +1,28 @@
+package se.chalmers.tda367.dominion.server;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryonet.EndPoint;
+
+public class NetworkCommon {
+
+	public static final int PORT = 54555;
+	
+	public static void register(EndPoint ep){
+		Kryo kryo = ep.getKryo();
+		//TODO Viktigt att registrera alla klasser här.
+		kryo.register(BasicMessage.class);
+	}
+	
+	public static class BasicMessage{
+		private String message;
+		
+		public String getMessage(){
+			return message;
+		}
+		
+		public void setMessage(String message){
+			this.message = message;
+		}
+	}
+
+}
