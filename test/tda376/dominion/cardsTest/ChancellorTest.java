42b6baec5cbd61d3edbccc93e0af972d42ef2fb2
Wiixtor <wiixtor@gmail.com>
2013-04-22 15:47:36 +0200

    Added ChancellorTest

---
+++ test/tda376/dominion/cardsTest/ChancellorTest.java
@@ -0,0 +1,27 @@
+package tda376.dominion.cardsTest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import tda376.dominion.cards.Chancellor;
+import tda376.dominion.model.Player;
+
+public class ChancellorTest {
+
+	@Test
+	public void testPlay() {
+		Player p = new Player("Heart attack man");
+		int money = p.getMoney();
+		Chancellor.play(p);
+		assertTrue(p.getMoney() - money == 2);		
+	}
+	
+	@Test 
+	public void testDiscardDeck() {
+		Player p = new Player("Heart attack man #2");
+		Chancellor.discardDeck(p);
+		assertTrue(p.getDeckSize() == 0);
+	}
+
+}
