683e731e5f1aa8e689553715f6a97912182c2b66
Malm <malmstenen@gmail.com>
2013-04-23 12:50:34 +0200

    Methods and variables for ConnectionMessage

--- src/tda367/dominion/messages/ConnectionMessage.java
+++ src/tda367/dominion/messages/ConnectionMessage.java
@@ -1,7 +1,16 @@
 package tda367.dominion.messages;
 
-public class ConnectionMessage {
-
+/**
+ * A message class for connecting to the server
+ */
+public class ConnectionMessage implements Message{
+	private final String name;
 	
-
+	public ConnectionMessage(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
 }
