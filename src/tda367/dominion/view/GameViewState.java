b08a94a76fe5faa967b16f5d51183e73c8bbe109
Wiixtor <wiixtor@gmail.com>
2013-04-23 14:42:33 +0200

    Added a few test views

---
+++ src/tda367/dominion/view/GameViewState.java
@@ -0,0 +1,37 @@
+package tda367.dominion.view;
+
+import org.newdawn.slick.*;
+import org.newdawn.slick.state.*;
+
+public class GameViewState extends BasicGameState {
+
+	Image hrtatk = null;
+	public static final int ID = 0;
+	
+	@Override
+	public void init(GameContainer arg0, StateBasedGame arg1)
+			throws SlickException {
+		hrtatk = new Image("res/img/card/bonus/Heartattack.png");
+		
+	}
+
+	@Override
+	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2)
+			throws SlickException {
+		hrtatk.draw(200, 200);
+		
+	}
+
+	@Override
+	public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
+			throws SlickException {
+		s
+		
+	}
+
+	@Override
+	public int getID() {
+		return ID;
+	}
+
+}
