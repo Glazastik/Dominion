8a6ce4d8fa619077ca0ecd4594428d78ba625745
Malm <malmstenen@gmail.com>
2013-04-24 16:37:00 +0200

    Better names

--- src/tda367/dominion/client/ClientMain.java
+++ src/tda367/dominion/client/ClientMain.java
@@ -2,7 +2,7 @@ package tda367.dominion.client;
 
 public class ClientMain {
 	public static void main(String[] args) {
-		ClientFactory client = new ClientFactory();
-		client.createClient();
+		ClientFactory connection = new ClientFactory();
+		connection.createClient();
 	}
 }
