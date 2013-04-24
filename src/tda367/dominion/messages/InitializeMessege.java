6ab283e82d669e5a71d26edacc1660ff0038e416
Meddan <christoffer.medin@gmail.com>
2013-04-24 11:20:54 +0200

    Added a bit of documentation.

--- src/tda367/dominion/messages/InitializeMessege.java
+++ src/tda367/dominion/messages/InitializeMessege.java
@@ -3,7 +3,14 @@ package tda367.dominion.messages;
 import java.util.HashMap;
 import java.util.LinkedList;
 
+/**
+ * A class sent by the server to the client to initialize a game.
+ * The data contained in the message should be enough for the client to start up the graphical view and such.
+ * @author christoffer
+ *
+ */
 public class InitializeMessege implements Message {
+	//TODO: More data to add.
 	private HashMap<String, Integer> cardsInSupply;
 	private LinkedList<String> players;
 	public InitializeMessege(HashMap<String,Integer> cardsInSupply, LinkedList<String> players) {
