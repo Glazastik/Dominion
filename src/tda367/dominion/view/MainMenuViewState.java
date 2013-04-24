b301a1d97a5e7b0f23535e64f2b65371b52f4f1f
Glazastik <anton.myrholm@gmail.com>
2013-04-24 16:48:46 +0200

    Modified MainmenuView

--- src/tda367/dominion/view/MainMenuViewState.java
+++ src/tda367/dominion/view/MainMenuViewState.java
@@ -12,28 +12,35 @@ public class MainMenuViewState extends BasicGameState {
 	String mouse = "";
 	Image exitButton = null;
 	Image playButton = null;
+	Image serverButton = null;
 	Image options = null;
 	Image background = null;
+
 	Rectangle playRec = null;
 	Rectangle exitRec = null;
+	Rectangle serverRec = null;
 	Rectangle optionsRec = null;
 
 	int id = 0;
-	
+
 	public MainMenuViewState(int id) {
 		this.id = id;
 	}
-	
+
 	@Override
 	public void init(GameContainer arg0, StateBasedGame arg1)
 			throws SlickException {
 		exitButton = new Image("res/img/gui/menu/exitGame.png");
 		playButton = new Image("res/img/gui/menu/playGame.png");
+		serverButton = new Image("res/img/gui/menu/playGame.png")
+				.getFlippedCopy(true, true);
 		options = new Image("res/img/gui/menu/options.png");
 		background = new Image("res/img/gui/menu/background.jpg");
+
 		playRec = new Rectangle(100, 350, 200, 50);
 		exitRec = new Rectangle(100, 150, 200, 50);
 		optionsRec = new Rectangle(100, 250, 200, 50);
+		serverRec = new Rectangle(100, 500, 200, 50);
 
 	}
 
@@ -41,11 +48,12 @@ public class MainMenuViewState extends BasicGameState {
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
 		background.draw();
-		g.drawString("Main Menu "+mouse , 0, 0);
+		g.drawString("Main Menu " + mouse, 0, 0);
 		exitButton.draw(100, 400);
 		playButton.draw(100, 200);
+		serverButton.draw(100, 500);
 		options.draw(100, 300);
-		
+
 	}
 
 	@Override
@@ -55,20 +63,29 @@ public class MainMenuViewState extends BasicGameState {
 		int xPos = Mouse.getX();
 		int yPos = Mouse.getY();
 		mouse = "X: " + xPos + " Y: " + yPos;
-		
-		//Checks if mouse cursor is within playgame image
-		if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON) && playRec.contains(xPos, yPos)) {
-				sbg.enterState(0, null, createNewHorizontalSplitTransition());
+
+		// Checks if mouse cursor is within playgame image
+		if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)
+				&& playRec.contains(xPos, yPos)) {
+			sbg.enterState(0, null, createNewHorizontalSplitTransition());
 		}
 
-		//Checks if mouse cursor is within exitgame image
-		if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON) && exitRec.contains(xPos, yPos)) {
+		// Checks if mouse cursor is within exitgame image
+		if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)
+				&& exitRec.contains(xPos, yPos)) {
 			sbg.getContainer().exit();
 		}
-		
-		//Checks if mouse cursor is within options image
-		if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON) && optionsRec.contains(xPos, yPos)) {
+
+		// Checks if mouse cursor is within options image
+		if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)
+				&& optionsRec.contains(xPos, yPos)) {
 			sbg.enterState(2, null, createNewSelectTransition());
+		}
+
+		// Checks if mouse cursor is within server image
+		if (input.isMouseButtonDown(Input.MOUSE_LEFT_BUTTON)
+				&& serverRec.contains(xPos, yPos)) {
+			sbg.enterState(3, null, createNewSelectTransition());
 		}
 	}
 
@@ -76,27 +93,27 @@ public class MainMenuViewState extends BasicGameState {
 	public int getID() {
 		return id;
 	}
-	
+
 	public Transition createNewHorizontalSplitTransition() {
 		Transition splitTransition = null;
 		try {
 			splitTransition = HorizontalSplitTransition.class.newInstance();
 		} catch (Throwable e) {
-		      Log.error(e);
-	    }
-		
+			Log.error(e);
+		}
+
 		return splitTransition;
 	}
-	
+
 	public Transition createNewSelectTransition() {
 		Transition selectTransition = null;
 		try {
 			selectTransition = SelectTransition.class.newInstance();
 		} catch (Throwable e) {
-		      Log.error(e);
-	    }
-		
+			Log.error(e);
+		}
+
 		return selectTransition;
 	}
-	
+
 }
