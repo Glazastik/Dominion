3374cbe1f31e1f0011357ab5af04468a09b719ff
Glazastik <anton.myrholm@gmail.com>
2013-04-24 10:43:45 +0200

    Added Client.package and ClientMain.class

---
+++ src/tda367/dominion/client/ClientMain.java
@@ -0,0 +1,13 @@
+package tda367.dominion.client;
+
+public class ClientMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
