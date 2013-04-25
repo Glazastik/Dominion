3674dac4f7fcf005ec38e0460c9fed6b9eb14a81
Pontus Doverstav <doverstav@hotmail.com>
2013-04-26 00:01:38 +0200

    Refactored code regarding the 5 random cards to improve readability and to try to comply with standards
    A lot of this code is probably temporary and will be deleted in a later
    stage, it's main purpose is for testing and experimenting to find
    restrictions and pass any and all hurdles.

--- src/tda367/dominion/view/InGameViewState.java
+++ src/tda367/dominion/view/InGameViewState.java
@@ -23,32 +23,19 @@ public class InGameViewState extends BasicGameState {
 		board = new Image("res/img/gui/ingame/BoardTemp.png");
 		cardsToShow = new Image[5];
 		
-		cih = CardInfoHandler.getInstance();
 		
-		List<String> cardList = cih.getActionCards();
-		Random r = new Random();
 		for(int i = 0; i < cardsToShow.length; i++){
-			cardsToShow[i] = new Image(cih.getImageLink(cardList.get(r.nextInt(cardList.size()-1))));//Who says nestled code is bad?
+			cardsToShow[i] = getRandomCard();
 		}
 		
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
-			throws SlickException {
-		int cardWidth;
-		double scale;
-		int cardHeight;
-		
+			throws SlickException {		
 		g.drawString("InGameState", 0, 0);
 		board.draw();
-		
-		cardWidth = gc.getWidth()/5;
-		for(int i = 0; i < cardsToShow.length; i++){
-			scale = (double)cardWidth/cardsToShow[i].getWidth();
-			cardHeight = (int) (cardsToShow[i].getHeight()*scale);
-			cardsToShow[i].draw(cardWidth*i, 0, cardWidth, cardHeight);
-		}
+		paintCardArray(gc);
 		
 	}
 
@@ -67,6 +54,41 @@ public class InGameViewState extends BasicGameState {
 	@Override
 	public int getID() {
 		return id;
+	}
+	
+	/**
+	 * Returns a random card of any type
+	 * 
+	 * @return a randomly selected card from the list of cards
+	 * @throws SlickException
+	 */
+	private Image getRandomCard() 
+			throws SlickException {
+		cih = CardInfoHandler.getInstance();
+		
+		List<String> cardList = cih.getCardList();
+		Random r = new Random();
+		
+		int index = r.nextInt(cardList.size()-1);
+		return new Image(cih.getImageLink(cardList.get(index)));	
+	}
+	
+	/**
+	 * Prints the entire array filled with cards.
+	 * 
+	 * <p>This method (and the array filled with cards),
+	 * is mainly for demonstrative and experimental
+	 * purposes and will probably become obsolete or
+	 * have to go trough extensive modification.
+	 */
+	private void paintCardArray(GameContainer gc) {	
+		
+		int cardWidth = gc.getWidth()/5;
+		for(int i = 0; i < cardsToShow.length; i++){
+			double scale = (double)cardWidth/cardsToShow[i].getWidth();
+			int cardHeight = (int) (cardsToShow[i].getHeight()*scale);
+			cardsToShow[i].draw(cardWidth*i, 0, cardWidth, cardHeight);
+		}
 	}
 
 }
