10b55cf5a78ff06c9e2db9d1fb0184d2fff0cc5d
Meddan <christoffer.medin@gmail.com>
2013-04-25 10:24:51 +0200

    Created chapel.

--- src/tda367/dominion/cards/Cellar.java
+++ src/tda367/dominion/cards/Cellar.java
@@ -8,7 +8,7 @@ public class Cellar {
 		int amountDiscarded = 0;
 		p.increaseActions(1);
 		//p.sendMessage("Discard any number of cards and then draw that many cards.")
-//		while(p.getNextMessage().instanceOf(doneMessage) || p.getHandSize() == 0){
+//		while(!p.getNextMessage().instanceOf(doneMessage) || p.getHandSize() == 0){
 //			if(p.getNextMessage().instanceOf(LocatedCardMessage)){
 //				LocatedCardMessage temp = (LocatedCardMessage)p.getNextMessage();
 //				if((temp.getLocation.equals("Hand") && p.getHand().contains(temp.getCardName()){
---
+++ src/tda367/dominion/cards/Chapel.java
@@ -0,0 +1,21 @@
+package tda367.dominion.cards;
+
+import tda367.dominion.model.Player;
+
+public class Chapel {
+
+	public static void play(Player p){
+		int amountTrashed = 0;
+	//p.sendMessage("Trash up to 4 cards from your hand")
+//	while(!p.getNextMessage().instanceOf(doneMessage) || trashedCards < 4 ){
+//		if(p.getNextMessage().instanceOf(LocatedCardMessage)){
+//			LocatedCardMessage temp = (LocatedCardMessage)p.getNextMessage();
+//			if((temp.getLocation.equals("Hand") && p.getHand().contains(temp.getCardName()){
+//				p.trash(temp.getCardName());
+//				amountTrashed++;
+//			}
+//		}
+//	}
+	}
+
+}
