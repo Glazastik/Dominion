0521ee7473adba1061c2da02a78138ee6752905e
Pontus Doverstav <doverstav@hotmail.com>
2013-04-25 16:47:24 +0200

    Added documentation for the Transisitons class

--- src/tda367/dominion/view/Transitions.java
+++ src/tda367/dominion/view/Transitions.java
@@ -3,10 +3,29 @@ package tda367.dominion.view;
 import org.newdawn.slick.state.transition.*;
 import org.newdawn.slick.util.Log;
 
+/**
+ * The purpose of this class is to provide an easy 
+ * way to use transitions without cluttering the
+ * code with try-catch-blocks.
+ * 
+ * <p>The way it does this is quite simple. It has 
+ * a static method for every transition that might be
+ * used. These methods take care of exception handling
+ * and returns a new instance of every transition, 
+ * which can be used once and only once.<p>
+ * 
+ * @author Group 28
+ *
+ */
+
 public class Transitions {
 
 	/**
-	 * Returns a new instance of a SelectTransition
+	 * Returns a new instance of a SelectTransition.
+	 * 
+	 * <p>These transitions can be used once and only once,
+	 * so this method has to be called for every transition
+	 * used.<p>
 	 * 
 	 * @return an instance of a SelectTransition
 	 */
@@ -23,6 +42,10 @@ public class Transitions {
 	
 	/**
 	 * Returns a new instance of a HorizontalSplitTransition
+	 * 
+	 * <p>These transitions can be used once and only once,
+	 * so this method has to be called for every transition
+	 * used.<p>
 	 * 
 	 * @return an instance of a HorizontalSplitTransition
 	 */
