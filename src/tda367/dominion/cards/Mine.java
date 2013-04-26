a9e76c25d7b14a97753a020952823bc3e2e9d38f
Meddan <christoffer.medin@gmail.com>
2013-04-26 13:15:37 +0200

    Worked on Mine.

--- src/tda367/dominion/cards/Mine.java
+++ src/tda367/dominion/cards/Mine.java
@@ -9,10 +9,14 @@ public class Mine {
 
 	public static void play(Player p, Supply supply){
 		int trashedValue = 0;
+		boolean doneTrashing = false;
+		boolean doneGaining = false;
 		CardInfoHandler cif = CardInfoHandler.getInstance();
 		GainingHandler gH = new GainingHandler(supply);
 		if(p.getHand().contains("Copper") || p.getHand().contains("Silver") || p.getHand().contains("Gold") ){
+			//while(!doneTrashing){
 			//p.sendMessage("Trash a treasure from you hand.")
+			
 			//if(p.getNextMessage().instanceOf(LocatedCardMessage)){
 			//LocatedCardMessage temp = (LocatedCardMessage) p.getNextMessage();
 			
@@ -21,16 +25,22 @@ public class Mine {
 					//switch(tempCard){
 					//case "Copper"; p.trashCard("Copper"); trashedValue = 0; break;
 					//case "Silver"; p.trashCard("Silver"); trashedValue = 3; break;
-					//case "Gold"; p.trashCard("Gold"); trashedValue = 6; break;/			
+					//case "Gold"; p.trashCard("Gold"); trashedValue = 6; break;
+				//doneTrashing = true;
 				//}
 			//}
+			//}
 			
+			//while(!doneGaining){
 			//p.sendMessage("Gain a card costing up to 3 more.")
 			//if(p.getNextMessage().instanceOf(LocatedCardMessage)){
 			//LocatedCardMessage temp = (LocatedCardMessage) p.getNextMessage();
 			//tempCard = temp.getCardName();
 			//if(cif.getCardType(tempCard).equals("Treasure") && gH.isCardGainable(tempCard, 3 + trashedValue){
 			//gH.playerGainCard(p, tempCard);
+			//doneGaining = true;
+			//}
+			
 			//}
 			
 		}
