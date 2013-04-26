b2fc98c5cb6a5d55279d451a84d1fd6c5dc307a3
Meddan <christoffer.medin@gmail.com>
2013-04-26 14:41:41 +0200

    Updated MoneyLender and added documentation.

--- src/tda367/dominion/cards/Moneylender.java
+++ src/tda367/dominion/cards/Moneylender.java
@@ -1,11 +1,21 @@
 package tda367.dominion.cards;
 
 import tda367.dominion.model.Player;
-
+/**
+ * A class used to represent the card Moneylender
+ * @author christoffer
+ *
+ */
 public class Moneylender {
+	/**
+	 * Calling this method affects a player as if they played the card Moneylender.
+	 * For simplicitys sake the player has no choice in which copper he/she trashes. (It should not matter)
+	 * @param player the player that played the card
+	 */
 	public static void play(Player player){
 		if(player.getHand().contains("Copper")){
 			player.trashCard("Copper");
+			player.increaseMoney(3);
 		}
 	}
 }
