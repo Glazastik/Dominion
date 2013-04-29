bafc6991a189ca708ac77dda18c6b4e3b098b68b
Meddan <christoffer.medin@gmail.com>
2013-04-29 10:25:04 +0200

    Started work on thief.

---
+++ src/tda367/dominion/cards/Thief.java
@@ -0,0 +1,15 @@
+package tda367.dominion.cards;
+
+import java.util.LinkedList;
+
+import tda367.dominion.model.Player;
+
+public class Thief {
+	public static void play(Player p, LinkedList<Player> players){
+		for(Player player : players){
+			if(player!=p){
+				
+			}
+		}
+	}
+}
