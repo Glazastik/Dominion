f0954f6d9bfa6c2410e7c9d84519bb5e76b7e525
Glazastik <anton.myrholm@gmail.com>
2013-04-30 13:09:06 +0200

    Game does not crash when you open serverstate and does not have network

--- src/tda367/dominion/view/ServerListState.java
+++ src/tda367/dominion/view/ServerListState.java
@@ -12,55 +12,68 @@ import tda367.dominion.controller.ClientController;
 
 public class ServerListState extends ControlledGameState {
 	private ClientController controller;
-	
+	private String[][] roomData;
+
 	public ServerListState(int id, ClientController controller) {
 		super(id, controller);
 		this.controller = controller;
 	}
-	
+
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
-		
+		updateRoomData();
+	}
+
+	private void updateRoomData() {
+		roomData = controller.getRoomData();
+		if (roomData == null) {
+			roomData = new String[0][0];
+		}
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
 		g.setColor(Color.green);
-		g.fillRect(50, 50, gc.getWidth()-100, gc.getHeight()-100);
+		g.fillRect(50, 50, gc.getWidth() - 100, gc.getHeight() - 100);
 		g.setColor(Color.black);
-		g.drawString("Room 0:", 80, 50);
-		g.drawString("Name:" + controller.getRoomData()[0][0], 80, 60);
-		g.drawString("Slots: "+controller.getRoomData()[0][1], 80, 70);
-		g.drawString("ID: "+controller.getRoomData()[0][2], 80, 80);
+		for (int i = 0; i < roomData.length; i++) {
+			g.drawString("<Room " + roomData[0][2] + ">", 80, 50);
+			g.drawString("Name:" + roomData[0][0], 80, 60);
+			g.drawString("Slots: " + roomData[0][1], 80, 70);
+		}
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)
 			throws SlickException {
 		Input input = gc.getInput();
-		
-		//Enter menu
-		if (input.isKeyPressed(Input.KEY_SPACE) || input.isKeyPressed(Input.KEY_1)) {
-			sbg.enterState(1, null, Transitions.createNewHorizontalSplitTransition());
+
+		// Enter menu
+		if (input.isKeyPressed(Input.KEY_SPACE)
+				|| input.isKeyPressed(Input.KEY_1)) {
+			sbg.enterState(1, null,
+					Transitions.createNewHorizontalSplitTransition());
 		}
 
 	}
-	
+
 	/**
 	 * This method is called every time this state is left.
 	 * 
-	 * Its current behavior is that it clears the record of 
-	 * keys pressed, removing any unwanted behavior in states
-	 * switched to.
+	 * Its current behavior is that it clears the record of keys pressed,
+	 * removing any unwanted behavior in states switched to.
 	 * 
-	 * @param gc the {@link GameContainer} this state is contained in
-	 * @param sbg the {@link StateBasedGame} this state is a part of
+	 * @param gc
+	 *            the {@link GameContainer} this state is contained in
+	 * @param sbg
+	 *            the {@link StateBasedGame} this state is a part of
 	 */
 	@Override
-	public void leave(GameContainer gc, StateBasedGame sbg) throws SlickException {
-		  gc.getInput().clearKeyPressedRecord();
+	public void leave(GameContainer gc, StateBasedGame sbg)
+			throws SlickException {
+		gc.getInput().clearKeyPressedRecord();
 	}
 
 }
