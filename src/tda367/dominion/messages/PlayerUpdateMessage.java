e48d625152b9e1eed0270e2f4c8334c33074b1d8
Malm <malmstenen@gmail.com>
2013-05-02 09:38:16 +0200

    Created PlayerUpdateMessage

---
+++ src/tda367/dominion/messages/PlayerUpdateMessage.java
@@ -0,0 +1,36 @@
+package tda367.dominion.messages;
+
+/**
+ * A class that updates the stats for a player.
+ * 
+ * Sends the amounts of actions, buys and money.
+ */
+public class PlayerUpdateMessage implements Message {
+	private int actions;
+	private int buys;
+	private int money;
+
+	public int getActions() {
+		return actions;
+	}
+
+	public void setActions(int actions) {
+		this.actions = actions;
+	}
+
+	public int getBuys() {
+		return buys;
+	}
+
+	public void setBuys(int buys) {
+		this.buys = buys;
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+}
