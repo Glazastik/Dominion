9af3d540a7eb944b9cdaf10fb98728d73005f436
Meddan <christoffer.medin@gmail.com>
2013-05-03 15:29:31 +0200

    Updated CardRulesHandler.

---
+++ src/tda367/dominion/server/cards/Militia.java
@@ -0,0 +1,28 @@
+package tda367.dominion.server.cards;
+
+import java.util.LinkedList;
+
+import tda367.dominion.server.model.Player;
+
+public class Militia {
+
+	public static void play(Player player, LinkedList<Player> players){
+		player.increaseMoney(2);
+		for(Player p: players){
+			if(player!=p){
+				while(p.getHandSize()>3){
+					//p.sendInformationMessage("Discard down to 3 cards");
+					//Message temp = p.getNextMessage
+					//if(temp instanceOf LocatedCardMessage){
+						//LocatedCardMessage tempMessage = (LocatedCardMessage) temp;
+						//if(p.getHand().contains(tempMessage.getCard()) && tempMessage.getLocation().equals("Hand")){
+							//p.discardCard((LocatedCardMessage) temp.getCard)
+						//}
+					//}
+					//p.removeInformationMessage();
+				}
+			}
+		}
+	}
+
+}
--- src/tda367/dominion/server/model/CardRulesHandler.java
+++ src/tda367/dominion/server/model/CardRulesHandler.java
@@ -2,15 +2,27 @@ package tda367.dominion.server.model;
 
 import java.util.LinkedList;
 
+import tda367.dominion.server.cards.Adventurer;
+import tda367.dominion.server.cards.Cellar;
+import tda367.dominion.server.cards.Chapel;
 import tda367.dominion.server.cards.Councilroom;
+import tda367.dominion.server.cards.Feast;
 import tda367.dominion.server.cards.Festival;
 import tda367.dominion.server.cards.Laboratory;
+import tda367.dominion.server.cards.Library;
 import tda367.dominion.server.cards.Market;
+import tda367.dominion.server.cards.Militia;
+import tda367.dominion.server.cards.Mine;
 import tda367.dominion.server.cards.Moat;
+import tda367.dominion.server.cards.Moneylender;
+import tda367.dominion.server.cards.Remodel;
 import tda367.dominion.server.cards.Smithy;
+import tda367.dominion.server.cards.Spy;
+import tda367.dominion.server.cards.Thief;
+import tda367.dominion.server.cards.Throneroom;
 import tda367.dominion.server.cards.Village;
 import tda367.dominion.server.cards.Witch;
-import tda367.dominion.server.cards.Woodcutter;
+import tda367.dominion.server.cards.Workshop;
 
 /**
  * A class that handles how the cards that are played affect the game and players.
@@ -43,24 +55,31 @@ public class CardRulesHandler {
 	 */
 	public void playCard(Player player, String cardName) {
 		player.play(cardName);
-		if (cardName.equals("Smithy")) {
-			Smithy.play(player);
-		} else if (cardName.equals("Village")) {
-			Village.play(player);
-		} else if (cardName.equals("Councilroom")) {
-			Councilroom.play(player, players);
-		} else if (cardName.equals("Laboratory")) {
-			Laboratory.play(player);
-		} else if (cardName.equals("Market")) {
-			Market.play(player);
-		} else if (cardName.equals("Festival")) {
-			Festival.play(player);
-		} else if (cardName.equals("Witch")) {
-			Witch.play(player, players, supply);
-		} else if (cardName.equals("Woodcutter")) {
-			Woodcutter.play(player);
-		} else if (cardName.equals("Moat")) {
-			Moat.play(player);
+		switch(cardName){
+			case "Adventurer": Adventurer.play(player); break;
+			case "Cellar": Cellar.play(player); break;
+			case "Chapel": Chapel.play(player); break;
+			case "Copper": player.increaseMoney(1); break
+			case "Councilroom": Councilroom.play(player, players); break;
+			case "Feast": Feast.play(player, supply); break;
+			case "Festival": Festival.play(player); break;
+			case "Gold": player.increaseMoney(3); break
+			case "Laboratory": Laboratory.play(player); break;
+			case "Library": Library.play(player); break;
+			case "Market": Market.play(player); break;
+			case "Militia": Militia.play(player, players); break;
+			case "Mine": Mine.play(player, supply); break;
+			case "Moat": Moat.play(player); break;
+			case "Moneylender": Moneylender.play(player); break;
+			case "Remodel": Remodel.play(player, supply); break;
+			case "Silver": player.increaseMoney(2); break
+			case "Smithy": Smithy.play(player); break;
+			case "Spy": Spy.play(player, players); break;
+			case "Thief": Thief.play(player, players); break;
+			case "Throneroom": Throneroom.play(player, players, supply); break;
+			case "Village": Village.play(player); break;
+			case "Witch": Witch.play(player, players, supply); break;
+			case "Workshop": Workshop.play(player, supply); break;
 		}
 	}
 }
