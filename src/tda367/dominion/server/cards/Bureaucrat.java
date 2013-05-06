54463ca4f8c5c61a915cd429928ba4dfa1c962ed
Meddan <christoffer.medin@gmail.com>
2013-05-06 16:17:49 +0200

    Added Moat support in Bureaucrat.

--- src/tda367/dominion/server/cards/Bureaucrat.java
+++ src/tda367/dominion/server/cards/Bureaucrat.java
@@ -12,11 +12,12 @@ public class Bureaucrat {
 	public static void play(Player player, LinkedList<Player> players, Supply supply){
 		GainingHandler gH = new GainingHandler(supply);
 		CardInfoHandler cif = CardInfoHandler.getInstance();
+		boolean hasVictoryCard = false;
 		if(gH.isCardGainable("Silver")){
 			player.putOnTopOfDeck(supply.take("Silver"));
 		}
 		for(Player p : players){
-			boolean hasVictoryCard = false;
+			hasVictoryCard = false;
 			if(p!=player){
 				Pile hand = p.getHand();
 				for(String card : hand.getCards()){
@@ -24,26 +25,48 @@ public class Bureaucrat {
 						hasVictoryCard = true;
 					}
 				}
-				if(hasVictoryCard){
-					boolean done = false;
-					//p.sendInformationMessage("Chose a victory card to put on top of your deck.");
-					while(!done){
-						//Message message = p.getNextMessage();
-						//if(message instanceOf LocatedCardMessage){
-							//LocatedCardMessage tempMessage = (LocatedCardMessage) message;
-							//if(tempMessage.getLocation().equals("Hand") && cif.getCardType(tempMessage.getCardName().equals("Victory"))){
-								//p.putOnTopOfDeck(p.getHand().pop(tempMessage.getCardName());)
-								//done = true;
-							//}
-						//}
-					}
-					//p.removeInformationMessage();
+				if(hasVictoryCard && !p.getHand().contains("Moat")){
+					Bureaucrat(player, p);
+				} else if(hasVictoryCard && p.getHand().contains("Moat")){
+					/**
+					 * p.sendInformationMessage("Do you wish to reveal Moat?");
+					 * p.createBoolMessage();
+					 * boolean done = false;
+					 * while(!done){
+					 * 	Message message = p.getNextMessage();
+					 * 	if(message instanceOf BoolMessage){
+					 * 		done = true;
+					 * 		BoolMessage boolMessage = (BoolMessage) message;
+					 * 		if(!boolMessage.isTrue()){
+					 * 			Bureaucrat(player,p);
+					 * 		}
+					 * 	}
+					 * }
+					 * p.removeInformationMessage();
+					 * p.removeBoolMessage(); 
+					 */
 				} else {
 					//player.sendMultipleCardMessage(p.getHand().getCards());
 					//wait(1000);
 					//player.removeMultipleCardMessage();
 				}
 			}
-		}
+		}	
+	}
+	private static void Bureaucrat(Player player, Player p){
+		CardInfoHandler cif = CardInfoHandler.getInstance();
+				boolean done = false;
+				//p.sendInformationMessage("Chose a victory card to put on top of your deck.");
+				while(!done){
+					//Message message = p.getNextMessage();
+					//if(message instanceOf LocatedCardMessage){
+						//LocatedCardMessage tempMessage = (LocatedCardMessage) message;
+						//if(tempMessage.getLocation().equals("Hand") && cif.getCardType(tempMessage.getCardName().equals("Victory"))){
+							//p.putOnTopOfDeck(p.getHand().pop(tempMessage.getCardName());)
+							//done = true;
+						//}
+					//}
+				}
+				//p.removeInformationMessage();
 	}
 }
--- src/tda367/dominion/server/cards/Militia.java
+++ src/tda367/dominion/server/cards/Militia.java
@@ -10,18 +10,41 @@ public class Militia {
 		player.increaseMoney(2);
 		for(Player p: players){
 			if(player!=p){
-				while(p.getHandSize()>3){
-					//p.sendInformationMessage("Discard down to 3 cards");
-					//Message temp = p.getNextMessage
-					//if(temp instanceOf LocatedCardMessage){
-						//LocatedCardMessage tempMessage = (LocatedCardMessage) temp;
-						//if(p.getHand().contains(tempMessage.getCard()) && tempMessage.getLocation().equals("Hand")){
-							//p.discardCard((LocatedCardMessage) temp.getCard)
-						//}
-					//}
-					//p.removeInformationMessage();
+				if(p.getHand().contains("Moat")){
+					Militia(p);
+				} else {
+					/**
+					 * p.sendInformationMessage("Do you wish to reveal Moat?");
+					 * p.createBoolMessage();
+					 * boolean done = false;
+					 * while(!done){
+					 * 	Message message = p.getNextMessage();
+					 * 	if(message instanceOf BoolMessage){
+					 * 		done = true;
+					 * 		BoolMessage boolMessage = (BoolMessage) message;
+					 * 		if(!boolMessage.isTrue()){
+					 * 			Militia(p);
+					 * 		}
+					 * 	}
+					 * }
+					 * p.removeInformationMessage();
+					 * p.removeBoolMessage(); 
+					 */
 				}
 			}
+		}
+	}
+	private static void Militia(Player p){
+		while(p.getHandSize()>3){
+			//p.sendInformationMessage("Discard down to 3 cards");
+			//Message temp = p.getNextMessage
+			//if(temp instanceOf LocatedCardMessage){
+				//LocatedCardMessage tempMessage = (LocatedCardMessage) temp;
+				//if(p.getHand().contains(tempMessage.getCard()) && tempMessage.getLocation().equals("Hand")){
+					//p.discardCard((LocatedCardMessage) temp.getCard)
+				//}
+			//}
+			//p.removeInformationMessage();
 		}
 	}
 
