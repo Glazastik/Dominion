f49d6947b10007f5bc7bfb0713d21c8fe31004a9
Malm <malmstenen@gmail.com>
2013-05-08 21:14:58 +0200

    Made ClientMain much more lightweight

--- src/tda367/dominion/client/main/ClientMain.java
+++ src/tda367/dominion/client/main/ClientMain.java
@@ -1,16 +1,11 @@
 package tda367.dominion.client.main;
 
 import tda367.dominion.client.controller.ClientController;
-import tda367.dominion.client.model.ClientModel;
-import tda367.dominion.client.network.ClientConnection;
-import tda367.dominion.client.view.MainView;
 
 public class ClientMain {
-	private static ClientController controller;
 	
 	public static void main(String[] args) {
-
-		controller = new ClientController();
+		new ClientController();
 	}
 	
 }
