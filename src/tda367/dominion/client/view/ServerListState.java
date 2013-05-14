8234a54f570b8c34b3513cc1de1bab652488b432
Glazastik <anton.myrholm@gmail.com>
2013-05-14 16:42:25 +0200

    ServerListState now shows rooms

--- src/tda367/dominion/client/view/ServerListState.java
+++ src/tda367/dominion/client/view/ServerListState.java
@@ -24,21 +24,21 @@ public class ServerListState extends ControlledGameState {
 	private Image headerBar;
 	private Image roomList[];
 	private TextField tf;
-	
+
 	private Rectangle joinRec;
 	private Rectangle hostRec;
 	private Rectangle refreshRec;
 	private Rectangle backRec;
 	private Rectangle roomRecs[];
-	
+
 	// Listeners
 	private GameListener roomUpdateListener;
 	private GameListener joinListener;
 	private GameListener hostListener;
 
-	//TODO: Temporary rectangle
+	// TODO: Temporary rectangle
 	private Rectangle join;
-	
+
 	private boolean leave = false;
 
 	public ServerListState(int id) {
@@ -59,34 +59,33 @@ public class ServerListState extends ControlledGameState {
 		hostRec = new Rectangle();
 		refreshRec = new Rectangle();
 		backRec = new Rectangle();
-		
+
 		roomRecs = new Rectangle[10];
-		for(int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++) {
 			roomRecs[i] = new Rectangle();
 		}
-		
+
 		roomList = new Image[10];
-		for(int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++) {
 			roomList[i] = new Image("res/img/gui/serverList/roomItem.png");
 		}
-		
-		
+
 		join = new Rectangle(600, 120, 250, 100);
 		tf = new TextField(gc, gc.getDefaultFont(), 1000, 50, 100, 35);
 		tf.setBackgroundColor(Color.white);
 		tf.setBorderColor(Color.black);
 		tf.setTextColor(Color.black);
 	}
-	
+
 	public void addUpdateRoomListener(GameListener l) {
 		roomUpdateListener = l;
 	}
-	
+
 	public void addJoinListener(GameListener l) {
 		joinListener = l;
 	}
-	
-	public void addHostListener(GameListener l){
+
+	public void addHostListener(GameListener l) {
 		hostListener = l;
 	}
 
@@ -95,17 +94,17 @@ public class ServerListState extends ControlledGameState {
 			roomData = s;
 		}
 	}
-	
+
 	private void updateRoomList() {
 		roomUpdateListener.run(new GameEvent());
 	}
-	
+
 	private void joinRoom(int roomId) {
 		GameEvent e = new GameEvent(roomId);
 		joinListener.run(e);
 	}
-	
-	private void hostRoom(){
+
+	private void hostRoom() {
 		hostListener.run(new GameEvent());
 	}
 
@@ -116,15 +115,8 @@ public class ServerListState extends ControlledGameState {
 		g.drawString("Name:", 950, 50);
 		tf.render(gc, g);
 		paintButtons();
-		paintRoomList();
-		
-		for (int i = 0; i < roomData.length; i++) {
-			room.draw(60, 40);
-			g.drawString("" + roomData[0][0], 110, 90);
-			g.drawString("" + roomData[0][1] + "/4", 110, 210);
-			g.fillRect(join.getX(), join.getY(), join.getWidth(),
-					join.getHeight());
-		}
+		paintRoomList(g);
+
 	}
 
 	@Override
@@ -151,7 +143,7 @@ public class ServerListState extends ControlledGameState {
 		super.enter(container, game);
 		System.out.println("Entered roomListState");
 		updateRoomList();
-//		controller.searchForGame();
+		// controller.searchForGame();
 	}
 
 	/**
@@ -172,36 +164,43 @@ public class ServerListState extends ControlledGameState {
 	}
 
 	/**
-	 * @see org.newdawn.slick.state.BasicGameState#mouseClicked(int, int, int, int)
+	 * @see org.newdawn.slick.state.BasicGameState#mouseClicked(int, int, int,
+	 *      int)
 	 */
 	@Override
 	public void mouseClicked(int button, int x, int y, int clickCount) {
-		
-		for(int i = 0; i < roomList.length; i++) {
-			if(roomRecs[i].contains(x, y)) {
+
+		for (int i = 0; i < roomList.length; i++) {
+			if (roomRecs[i].contains(x, y)) {
 				try {
 					resetRoomListImages();
-					roomList[i] =  new Image("res/img/gui/serverList/selectedRoomItem.png");
-					//TODO: set join ID 
+					roomList[i] = new Image(
+							"res/img/gui/serverList/selectedRoomItem.png");
+					// TODO: set join ID
 				} catch (SlickException e) {
 					e.printStackTrace();
 				}
 			}
 		}
-		
-		if(join.contains(x, y)){
-//			controller.joinRoom(0);
+
+		if (join.contains(x, y)) {
+			// controller.joinRoom(0);
 		}
-		
-		if(backRec.contains(x, y)) {
+
+		if (backRec.contains(x, y)) {
 			leave = true;
 		}
-		
-		if(refreshRec.contains(x, y)) {
+
+		if (refreshRec.contains(x, y)) {
 			updateRoomList();
+			try {
+				resetRoomListImages();
+			} catch (SlickException e) {
+				e.printStackTrace();
+			}
 		}
-		
-		if(hostRec.contains(x, y)){
+
+		if (hostRec.contains(x, y)) {
 			hostRoom();
 		}
 	}
@@ -211,52 +210,58 @@ public class ServerListState extends ControlledGameState {
 	 */
 	@Override
 	public void keyPressed(int key, char c) {
-		
+
 		super.keyPressed(key, c);
-		if(key == Input.KEY_F5){
+		if (key == Input.KEY_F5) {
 			updateRoomList();
 		}
-		
-		if(key == Input.KEY_1) {
+
+		if (key == Input.KEY_1) {
 			leave = true;
 		}
 	}
-	
+
 	private void resetRoomListImages() throws SlickException {
-		for(int i = 0; i < roomList.length; i++) {
+		for (int i = 0; i < roomData.length; i++) {
 			roomList[i] = new Image("res/img/gui/serverList/roomItem.png");
 		}
 	}
-	
-	private void paintRoomList() 
-			throws SlickException {
+
+	private void paintRoomList(Graphics g) throws SlickException {
 		int yOffset = 125;
 		int xOffset = 100;
 		int itemHeight = 48;
 		int itemWidth = 500;
 		int spacing = 5;
 		headerBar.draw(xOffset, yOffset);
-		for(int i = 0; i < roomList.length; i++) {
-			roomList[i].draw(xOffset , yOffset + (i+1)*itemHeight + (1+i)*spacing);
-			roomRecs[i].setBounds(xOffset, yOffset + (i+1)*itemHeight + (1+i)*spacing, itemWidth, itemHeight);
+		for (int i = 0; i < roomData.length; i++) {
+			roomList[i].draw(xOffset, yOffset + (i + 1) * itemHeight + (1 + i)
+					* spacing);
+			roomRecs[i].setBounds(xOffset, yOffset + (i + 1) * itemHeight
+					+ (1 + i) * spacing, itemWidth, itemHeight);
+			g.drawString(roomData[i][0], xOffset + 15, yOffset + (i + 1)
+					* itemHeight + (1 + i) * spacing + 15);
+			g.drawString(roomData[i][1], xOffset + 45, yOffset + (i + 1)
+					* itemHeight + (1 + i) * spacing + 15);
 		}
 	}
-	
-	private void paintButtons() 
-			throws SlickException {
+
+	private void paintButtons() throws SlickException {
 		int xOffset = 480;
 		int yOffset = 50;
 		int buttonHeight = joinButton.getHeight();
 		int buttonWidth = joinButton.getWidth();
 		int buttonSpacing = 10;
 		refreshButton.draw(xOffset, yOffset);
-		joinButton.draw(xOffset+buttonSpacing+buttonWidth, yOffset);
-		hostButton.draw(xOffset+2*buttonSpacing+2*buttonWidth, yOffset);
-		backButton.draw(50,50);
+		joinButton.draw(xOffset + buttonSpacing + buttonWidth, yOffset);
+		hostButton.draw(xOffset + 2 * buttonSpacing + 2 * buttonWidth, yOffset);
+		backButton.draw(50, 50);
 		refreshRec.setBounds(xOffset, yOffset, buttonWidth, buttonHeight);
-		joinRec.setBounds(xOffset+buttonSpacing+buttonWidth, yOffset, buttonWidth, buttonHeight);
-		hostRec.setBounds(xOffset+2*buttonSpacing+2*buttonWidth, yOffset, buttonWidth, buttonHeight);
-		backRec.setBounds(50,50,buttonWidth,buttonHeight);
+		joinRec.setBounds(xOffset + buttonSpacing + buttonWidth, yOffset,
+				buttonWidth, buttonHeight);
+		hostRec.setBounds(xOffset + 2 * buttonSpacing + 2 * buttonWidth,
+				yOffset, buttonWidth, buttonHeight);
+		backRec.setBounds(50, 50, buttonWidth, buttonHeight);
 	}
 
 }
