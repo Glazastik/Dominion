47e785bf5e150e523d768a85c43698435efa8c5f
Glazastik <anton.myrholm@gmail.com>
2013-05-16 14:25:54 +0200

    Added comments in Dominion

--- src/tda367/dominion/server/game/Dominion.java
+++ src/tda367/dominion/server/game/Dominion.java
@@ -9,25 +9,27 @@ import tda367.dominion.commons.messages.SupplyMessage;
 import tda367.dominion.server.network.NetworkHandler;
 
 /**
- * @author Group 28
- * Master class for one instance of a game.
+ * @author Group 28 Master class for one instance of a game.
  * 
  */
 public class Dominion {
 	// Player related
 	private final LinkedList<Player> players;
 	private int activePlayer;
-	
+
 	private final Supply supply;
 	private final CardRulesHandler cardRulesHandler;
 	private NetworkHandler network;
-	
+
 	/**
 	 * Constructs a fine game of Dominion!
-	 * @param players list of players playing the game
-	 * @param supply the supply depot for the piles
+	 * 
+	 * @param players
+	 *            list of players playing the game
+	 * @param supply
+	 *            the supply depot for the piles
 	 */
-	public Dominion(LinkedList<Player> players){
+	public Dominion(LinkedList<Player> players) {
 		this.players = players;
 		this.supply = new Supply(players.size());
 		cardRulesHandler = new CardRulesHandler(players, supply);
@@ -35,67 +37,78 @@ public class Dominion {
 		System.out.println("Game started");
 		init();
 	}
-	
+
+	/**
+	 * Initialize the game, send messages to all involved players.
+	 */
 	private void init() {
 		SetupMessage msg = new SetupMessage();
 		SupplyMessage smsg = new SupplyMessage();
-		
+
 		smsg.setSupply(supply.getCardsInSupply());
-		
+
 		String[] s = new String[players.size()];
-		
-		for(int i = 0; i < players.size(); i++) {
+
+		for (int i = 0; i < players.size(); i++) {
 			s[i] = players.get(i).getName();
 		}
-		
+
 		msg.setPlayers(s);
 		msg.setSupply(smsg);
-		
+
 		sendToAll(msg);
-		
-		for(Player p : players) {
+
+		for (Player p : players) {
 			p.updateCards();
 			p.updateStats();
 		}
 	}
-	
+
+	/**
+	 * Sends a particular message to all connected players.
+	 * 
+	 * @param msg
+	 */
 	private void sendToAll(Message msg) {
-		for(Player p : players) {
+		for (Player p : players) {
 			network.sendMessage(p.getID(), msg);
 		}
 	}
-	
+
 	/**
-	 * Returns a list of players (see {@link Player}) 
-	 * that are playing the game.
+	 * Returns a list of players (see {@link Player}) that are playing the game.
 	 * 
 	 * @return a list containing the players
 	 */
-	public LinkedList<Player> getPlayers(){
+	public LinkedList<Player> getPlayers() {
 		return this.players;
 	}
-	
+
 	/**
 	 * Returns the {@link Supply} used in the game.
 	 * 
 	 * @return the {@link Supply}
 	 */
-	public Supply getSupply(){
+	public Supply getSupply() {
 		return this.supply;
 	}
-	
-	
+
 	/**
 	 * Returns the {@link CardRulesHandler} used in the game.
 	 * 
 	 * @return the {@link CardRulesHandler}
 	 */
-	public CardRulesHandler getCardRulesHandler(){
+	public CardRulesHandler getCardRulesHandler() {
 		return this.cardRulesHandler;
 	}
 
+	/**
+	 * Returns a HashMap of the current cards in supply & their amounts.
+	 * 
+	 * @return
+	 */
 	public HashMap<String, Integer> getSupplyInfo() {
 		return supply.getCardsInSupply();
-		
-	}	
+
+	}
 }
