ac64df8bcb456afa3bcebf0bcdc8b97b0873fc19
Glazastik <anton.myrholm@gmail.com>
2013-05-22 11:19:28 +0200

    Forgot to commit PlayAllMessage

---
+++ src/tda367/dominion/commons/messages/PlayAllMessage.java
@@ -0,0 +1,8 @@
+package tda367.dominion.commons.messages;
+/**
+ * This message indicates that the client wants to play all his treasure cards.
+ *
+ */
+public class PlayAllMessage implements Message {
+	
+}
