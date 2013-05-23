9cc4b6424031583462c538e7625c927df100dfa7
Pontus Doverstav <doverstav@hotmail.com>
2013-05-23 16:16:37 +0200

    Started working on ending screen

---
+++ src/tda367/dominion/client/view/EndGameState.java
@@ -0,0 +1,38 @@
+package tda367.dominion.client.view;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class EndGameState extends BasicGameState {
+
+	@Override
+	public void init(GameContainer arg0, StateBasedGame arg1)
+			throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
+			throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(GameContainer gc, StateBasedGame sbg, int delta)
+			throws SlickException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getID() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
