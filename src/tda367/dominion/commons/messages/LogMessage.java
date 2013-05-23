135ce6d0c5838445da9b77baa9b58e3bb057810a
Glazastik <anton.myrholm@gmail.com>
2013-05-23 14:37:53 +0200

    Forgot to include LogMessage

---
+++ src/tda367/dominion/commons/messages/LogMessage.java
@@ -0,0 +1,25 @@
+package tda367.dominion.commons.messages;
+
+/**
+ * Tells the client(s) to update their tip box.
+ *
+ */
+public class LogMessage implements Message {
+	private String message;
+
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+}
