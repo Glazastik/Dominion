3ebc0681b94595f5783c3be050d9566caf2f8b6a
Wiixtor <wiixtor@gmail.com>
2013-05-24 15:23:31 +0200

    Added new ImageButton class to rule them all

---
+++ src/tda367/dominion/client/view/ImageButton.java
@@ -0,0 +1,39 @@
+package tda367.dominion.client.view;
+
+import org.lwjgl.util.Rectangle;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+import tda367.dominion.commons.listener.GameEvent;
+import tda367.dominion.commons.listener.GameListener;
+
+public class ImageButton extends Image {
+
+	private Rectangle rec;
+	private GameListener listener;
+
+	public ImageButton(String s) throws SlickException {
+		super(s);
+		rec = new Rectangle();
+		rec.setSize(this.getWidth(), this.getHeight());
+	}
+
+	public void contains(int x, int y) {
+		if (rec.contains(x, y)) {
+			listener.run(new GameEvent());
+		}
+	}
+	
+	public boolean boolContains(int x, int y) {
+		return rec.contains(x, y);
+	}
+	
+	public void addListener(GameListener l) {
+		listener = l;
+	}
+	
+	public void draw(int x, int y) {
+		super.draw(x, y);
+		rec.setLocation(x, y);
+	}
+}
