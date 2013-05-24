1a1297bf332aa14428c254ebbf27da7324768a21
Malm <malmstenen@gmail.com>
2013-05-24 12:55:22 +0200

    Made the RevealMessage implement Message

--- src/tda367/dominion/commons/messages/RevealMessage.java
+++ src/tda367/dominion/commons/messages/RevealMessage.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 /**
  * A message that tells the client the card that is revealed.
  */
-public class RevealMessage {
+public class RevealMessage implements Message{
 	private LinkedList<String> cards = new LinkedList<String>();
 	
 	public void addCard(String card) {
