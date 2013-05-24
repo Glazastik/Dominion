4714be111a210614b87dfb934a833901152ae848
Meddan <christoffer.medin@gmail.com>
2013-05-24 15:46:44 +0200

    Library first version (should be done).

--- src/tda367/dominion/server/game/cards/Library.java
+++ src/tda367/dominion/server/game/cards/Library.java
@@ -3,24 +3,27 @@ package tda367.dominion.server.game.cards;
 import tda367.dominion.commons.messages.BoolMessage;
 import tda367.dominion.commons.messages.Message;
 import tda367.dominion.server.game.CardInfoHandler;
+import tda367.dominion.server.game.Dominion;
 import tda367.dominion.server.game.Player;
 
 public class Library extends ChoiceCard{
-	public Library(){
+	private Dominion dominion;
+	private Player player; 
+	private CardInfoHandler cif = CardInfoHandler.getInstance();
+	public Library(Dominion dom){
+		this.dominion = dom;
 		state = State.NONACTIVE;
 	}
 	public void play(Player p){
+		player = p;
 		state = State.ACTIVE;
-		CardInfoHandler cif = CardInfoHandler.getInstance();
 		while(p.getHandSize() < 7){
 			if(p.revealTopOfDeck() == null){
 				state = State.NONACTIVE;
 				break;
 			} else if(cif.getCardType(p.revealTopOfDeck()).equals("Action")){
-				//p.s
-				//p.sendCardMessage(p.revealTopOfDeck());
-				//p.createBoolMessage(),
-				p.sendTip("Set aside: " + p.revealTopOfDeck() + "?");
+				p.reveal(p.revealTopOfDeck());
+				dominion.activateYesNoBox("Set aside?");
 				break;
 			} else {
 				p.draw();
@@ -44,6 +47,11 @@ public class Library extends ChoiceCard{
 		if(p.revealTopOfDeck() == null || p.getHandSize() >= 7){
 			p.putRevealedCardsInDiscard();
 			state = State.NONACTIVE;
+		} else if(cif.getCardType(p.revealTopOfDeck()).equals("Action")) {
+			p.reveal(p.revealTopOfDeck());
+			dominion.activateYesNoBox("Set aside?");
+		} else {
+			p.draw();
 		}
 	}
 
