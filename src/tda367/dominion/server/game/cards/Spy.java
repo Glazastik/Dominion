94a5090d2b91663e586901263fabf4a1f4b49e97
Wiixtor <wiixtor@gmail.com>
2013-05-25 17:37:36 +0200

    Updated spy

--- src/tda367/dominion/server/game/cards/Spy.java
+++ src/tda367/dominion/server/game/cards/Spy.java
@@ -10,68 +10,72 @@ import tda367.dominion.server.game.Dominion;
 import tda367.dominion.server.game.Player;
 
 public class Spy extends ChoiceCard {
-	private Dominion dominion;
 	private LinkedList<Player> players;
-	private HashMap<Player,Boolean> moatStatus;
+	private HashMap<Player, Boolean> moatStatus;
 	private Player activePlayer;
 	private LinkedList<Player> orderedPlayers;
 	private Player currentTarget;
 	Iterator<Player> i;
-	public Spy(Dominion dom){
-		this.dominion = dom;
+
+	public Spy(Dominion dom) {
+		game = dom;
 		state = State.NONACTIVE;
-		this.players = dominion.getPlayers();
-		moatStatus = new HashMap<Player,Boolean>();
-		for (Player p : players){
+		this.players = game.getPlayers();
+		activePlayer = game.getActivePlayer();
+		orderedPlayers = new LinkedList<Player>();
+		moatStatus = new HashMap<Player, Boolean>();
+		for (Player p : players) {
 			moatStatus.put(p, false);
 		}
 	}
-	public void play(Player player){
+
+	public void play() {
 		state = State.ACTIVE;
-		activePlayer = player;
-		int startingPos = players.indexOf(player);
-		for (int i = startingPos ; i < players.size() ; startingPos++){
+		int startingPos = players.indexOf(activePlayer);
+		for (int i = startingPos; i < players.size(); startingPos++) {
 			orderedPlayers.add(players.get(i));
 		}
-		for (int i = 0 ; i < startingPos ; i++){
+		for (int i = 0; i < startingPos; i++) {
 			orderedPlayers.add(players.get(i));
 		}
-		player.reveal(player.revealTopOfDeck());
-		dominion.activateYesNoBox("Discard this from top of your deck?");
-		
-		for(Player p: orderedPlayers){
-			if(p.getHand().contains("Moat") && player != p){
+		game.revealCard(activePlayer.revealTopOfDeck());
+		game.activateYesNoBox("Discard this from top of your deck?");
+
+		for (Player p : orderedPlayers) {
+			if (p.getHand().contains("Moat") && activePlayer != p) {
 				moatStatus.put(p, true);
 			}
 		}
-		currentTarget = player;
-		 i = orderedPlayers.iterator();
+		currentTarget = activePlayer;
+		i = orderedPlayers.iterator();
 	}
+
 	@Override
 	public void input(Message msg, Player p) {
-		if(msg instanceof BoolMessage){
-			if(((BoolMessage)msg).isTrue()){
+		if (msg instanceof BoolMessage) {
+			if (((BoolMessage) msg).isTrue()) {
 				currentTarget.discardTopOfDeck();
 			}
-			while(i.hasNext()){
+			while (i.hasNext()) {
 				currentTarget = i.next();
-				if(moatStatus.get(currentTarget) && i.hasNext()){
-					//NÄSTA TARGET
+				if (moatStatus.get(currentTarget) && i.hasNext()) {
+					// NÄSTA TARGET
 				} else if (!moatStatus.get(currentTarget)) {
-					p.reveal(currentTarget.revealTopOfDeck());
-					dominion.activateYesNoBox("Discard this from top of " + p.getName() + "'s deck?");
+					game.revealCard(currentTarget.revealTopOfDeck());
+					game.activateYesNoBox("Discard this from top of "
+							+ p.getName() + "'s deck?");
 					break;
-				} else if (!i.hasNext()){
+				} else if (!i.hasNext()) {
 					p.sendTip("Continue playing action cards.");
 					state = State.NONACTIVE;
 				}
 			}
-			
+
 		}
-		if(!i.hasNext()){
+		if (!i.hasNext()) {
 			p.sendTip("Continue playing action cards.");
 			state = State.NONACTIVE;
 		}
-		
+
 	}
 }
