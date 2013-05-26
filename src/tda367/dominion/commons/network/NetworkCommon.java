f9dc325c300c7773177a5daa744ab11f89915d7a
Wiixtor <wiixtor@gmail.com>
2013-05-26 17:29:59 +0200

    Added revealMessage to the register

--- src/tda367/dominion/commons/network/NetworkCommon.java
+++ src/tda367/dominion/commons/network/NetworkCommon.java
@@ -43,6 +43,7 @@ public class NetworkCommon {
 		kryo.register(AdvanceMessage.class);
 		kryo.register(TipMessage.class);
 		kryo.register(LogMessage.class);
+		kryo.register(RevealMessage.class);
 	}
 
 }
