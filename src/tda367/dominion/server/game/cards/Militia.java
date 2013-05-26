8f72f7da455a680f12b29a457bdb56f17169cfc0
Meddan <christoffer.medin@gmail.com>
2013-05-26 15:06:07 +0200

    Removed printing in militia.

--- src/tda367/dominion/server/game/cards/Militia.java
+++ src/tda367/dominion/server/game/cards/Militia.java
@@ -11,16 +11,16 @@ import tda367.dominion.server.game.Player;
 public class Militia extends ChoiceCard {
 	private LinkedList<Player> players;
 	private Player activePlayer;
-	private HashMap<Player, Boolean> notAffected;
+	private HashMap<Player, Boolean> moatStatus;
 	
 	public Militia (Dominion game) {
 		this.game = game;
 		this.activePlayer = game.getActivePlayer();
 		this.players = game.getPlayers();
 		state = State.NONACTIVE;
-		notAffected = new HashMap<Player, Boolean>();
+		moatStatus = new HashMap<Player, Boolean>();
 		for(Player p : players){
-			notAffected.put(p, false);
+			moatStatus.put(p, false);
 		}
 	} 
 
@@ -29,27 +29,21 @@ public class Militia extends ChoiceCard {
 		activePlayer = game.getActivePlayer();
 		activePlayer.increaseMoney(2);
 		LinkedList<Player> inactivePlayers = game.getInactivePlayers();
+
 		
-		for (Player p : inactivePlayers){
-			System.out.println("Inactive: " + p.getName());
-		}
-		
-		notAffected.put(activePlayer, true);
+		moatStatus.put(activePlayer, true);
 		for(Player p: inactivePlayers){
-			if(!p.getHand().contains("Moat") && p.getHandSize()>3){
-				System.out.println("PUTTING FALSE");
-				System.out.println(p.getName());
+			if(!p.getHand().contains("Moat") && p.getHandSize()>3){;
 				p.sendTip("Discard down to 3 cards");
-				notAffected.put(p, false);
+				moatStatus.put(p, false);
 			} else {
-				System.out.println("PUTTING TRUE");
-				notAffected.put(p, true);
+				moatStatus.put(p, true);
 			}
 		}
 		
 		boolean noOneDiscards = true;
 		for (Player p : inactivePlayers){
-			if(p.getHandSize()>3 && !notAffected.get(p)){
+			if(p.getHandSize()>3 && !moatStatus.get(p)){
 				noOneDiscards = false;
 			}
 		}
@@ -59,29 +53,25 @@ public class Militia extends ChoiceCard {
 	}
 	@Override
 	public void input(Message msg, Player player) {
+		System.out.println("Is in input");
 		boolean doneDiscarding = false;
 		
-		if(!notAffected.get(player)){
+		if(!moatStatus.get(player)){
 			if(msg instanceof CardMessage && player.getHandSize()>3){
 				if(player.hasCardInHand(((CardMessage) msg).getCard())){
 					player.discardCard(((CardMessage) msg).getCard());
 				}
 			}
 		}
+		boolean noOneDiscards = true;
 		for (Player p : players){
-			if(p == activePlayer || p.getHandSize() <= 3){
-				notAffected.put(p, true);
-			}
-		}
-		
-		
-		for (Player p : players){
-			if(p != activePlayer && notAffected.get(p)){
+			if(p != player && p.getHandSize()>3 && !moatStatus.get(p)){
+				noOneDiscards = false;
+			} else {
 				p.sendTip("Wait for " + activePlayer.getName() + " to finish playing action cards.");
 			}
 		}
-		
-		if(!notAffected.containsValue(false)){
+		if(noOneDiscards){
 			activePlayer.sendTip("Continue playing action cards.");
 			state = State.NONACTIVE;
 		}
